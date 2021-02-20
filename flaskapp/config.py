c4a677517627bd948cbf3a58130d99331964485e
d13y <daniel.jwhite@sky.com>
2021-02-20 20:12:15 +0000

    init

--- flaskapp/config.py
+++ flaskapp/config.py
@@ -4,12 +4,12 @@ import os
 class Config:
 
     # General configuration
-    SECRET_KEY = os.environ.get('EV_SECRET_KEY')  # key required for accounts
-    SQLALCHEMY_DATABASE_URI = os.environ.get('EV_SQL_DATABASE')  # database location
+    SECRET_KEY = os.environ.get('FLASK_SECRET_KEY')  # key required for accounts
+    SQLALCHEMY_DATABASE_URI = os.environ.get('FLASK_SQL_DATABASE')  # database location
 
     # Mail configuration
     MAIL_SERVER = 'smtp.googlemail.com'  # using gmail
     MAIL_PORT = '587'  # req port info
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get('EV_EMAIL_USER')  # gmail account
-    MAIL_PASSWORD = os.environ.get('EV_EMAIL_PASS')  # gmail password
+    MAIL_USERNAME = os.environ.get('FLASK_EMAIL_USER')  # gmail account
+    MAIL_PASSWORD = os.environ.get('FLASK_EMAIL_PASS')  # gmail password
